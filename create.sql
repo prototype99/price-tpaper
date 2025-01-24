214d7a6e7eb610361f1ecbac378821428cde720a
Seirra <sophietheopossum@yandex.ru>
2025-01-24 21:06:04 +0000

    add delivery column
    also add delivery cost for little duck

--- create.sql
+++ create.sql
@@ -5,11 +5,12 @@ CREATE TABLE data (
   sheets INTEGER(3) NOT NULL,
   uses INTEGER(4),
   cost REAL(2,2) NOT NULL,
+  delivery REAL(1,2) NOT NULL,
   useCost REAL(4,2),
   urla TEXT,
   urlb TEXT
 );
 UPDATE data SET uses = ( sheets * rolls ) / ( (CASE WHEN ply = 3 THEN 3 WHEN ply = 4 THEN 2 END) );
-UPDATE data SET useCost = cost / uses;
+UPDATE data SET useCost = ( cost + delivery ) / uses;
 
 SELECT * from data ORDER BY useCost;
